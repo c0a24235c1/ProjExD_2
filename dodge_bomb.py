62a2ef33ef7df37e132b761e01ddb76ec1149f40
c0a24235c1 <c0a24235c1@edu.teu.ac.jp>
2025-09-30 14:12:47 +0900

    直線移動する爆弾の製作

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -14,6 +15,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))  # 爆弾描写用の画像
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い爆弾を描写
+    bb_img.set_colorkey((0, 0, 0))  # 背景を透過
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾のX座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾のy座標
+    vx = 5  # 爆弾の横方向移動速度
+    vy = 5  # 爆弾の縦方向移動速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -31,8 +40,10 @@ def main():
                 sum_mv[1]+=mv[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
+        bb_rct.move_ip(vx,vy)  # 爆弾移動
         clock.tick(50)
 
 
